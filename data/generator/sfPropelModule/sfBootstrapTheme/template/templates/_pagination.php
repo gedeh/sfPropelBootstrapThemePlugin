fc65ed63c9608b6e367136714e7ae97ff5d7f965
Hendra Saputra <Gede.Saputra@verint.com>
2018-04-08 23:46:24 +0100

    list pagination use bootstrap v4.0

--- data/generator/sfPropelModule/sfBootstrapTheme/template/templates/_pagination.php
+++ data/generator/sfPropelModule/sfBootstrapTheme/template/templates/_pagination.php
@@ -1,43 +1,47 @@
-<nav aria-label="[?php echo __('Pagination') ?]">
-  <ul class="pagination justify-content-center">
-    <li class="page-item">
-      <a class="page-link" href="[?php echo url_for('@<?php echo $this->getUrlForAction('list') ?>') ?]?page=1" title="[?php echo __('First page', array(), 'sf_admin') ?]">
-        <span aria-hidden="true">&laquo;</span>
-        <span class="sr-only">[?php echo __('First page', array(), 'sf_admin') ?]</span>
-        <i class="icon-fast-backward"></i>
-      </a>
-    </li>
+<div class="row">
+  <div class="col">
+    <nav aria-label="[?php echo __('Pagination') ?]">
+      <ul class="pagination justify-content-center">
+        <li class="page-item">
+          <a class="page-link" href="[?php echo url_for('@<?php echo $this->getUrlForAction('list') ?>') ?]?page=1" title="[?php echo __('First page', array(), 'sf_admin') ?]">
+            <span aria-hidden="true">&laquo;</span>
+            <span class="sr-only">[?php echo __('First page', array(), 'sf_admin') ?]</span>
+            <i class="icon-fast-backward"></i>
+          </a>
+        </li>
 
-    <li class="page-item">
-      <a class="page-link" href="[?php echo url_for('@<?php echo $this->getUrlForAction('list') ?>') ?]?page=[?php echo $pager->getPreviousPage() ?]" title="[?php echo __('Previous page', array(), 'sf_admin') ?]">
-        <i class="icon-backward"></i>
-      </a>
-    </li>
+        <li class="page-item">
+          <a class="page-link" href="[?php echo url_for('@<?php echo $this->getUrlForAction('list') ?>') ?]?page=[?php echo $pager->getPreviousPage() ?]" title="[?php echo __('Previous page', array(), 'sf_admin') ?]">
+            <i class="icon-backward"></i>
+          </a>
+        </li>
 
-  [?php foreach ($pager->getLinks() as $page): ?]
-    [?php if ($page == $pager->getPage()): ?]
-      <li class="page-item active disabled">
-        <span class="page-link">[?php echo $page ?]</a>
-      </li>
-    [?php else: ?]
-      <li class="page-item">
-        <a class="page-link" href="[?php echo url_for('@<?php echo $this->getUrlForAction('list') ?>') ?]?page=[?php echo $page ?]">[?php echo $page ?]</a>
-      </li>
-    [?php endif; ?]
-  [?php endforeach; ?]
+      [?php foreach ($pager->getLinks() as $page): ?]
+        [?php if ($page == $pager->getPage()): ?]
+          <li class="page-item active disabled">
+            <span class="page-link">[?php echo $page ?]</a>
+          </li>
+        [?php else: ?]
+          <li class="page-item">
+            <a class="page-link" href="[?php echo url_for('@<?php echo $this->getUrlForAction('list') ?>') ?]?page=[?php echo $page ?]">[?php echo $page ?]</a>
+          </li>
+        [?php endif; ?]
+      [?php endforeach; ?]
 
-    <li class="page-item">
-      <a class="page-link" href="[?php echo url_for('@<?php echo $this->getUrlForAction('list') ?>') ?]?page=[?php echo $pager->getNextPage() ?]" title="[?php echo __('Next page', array(), 'sf_admin') ?]">
-        <i class="icon-forward"></i>
-      </a>
-    </li>
+        <li class="page-item">
+          <a class="page-link" href="[?php echo url_for('@<?php echo $this->getUrlForAction('list') ?>') ?]?page=[?php echo $pager->getNextPage() ?]" title="[?php echo __('Next page', array(), 'sf_admin') ?]">
+            <i class="icon-forward"></i>
+          </a>
+        </li>
 
-    <li class="page-item">
-      <a class="page-link" href="[?php echo url_for('@<?php echo $this->getUrlForAction('list') ?>') ?]?page=[?php echo $pager->getLastPage() ?]" title="[?php echo __('Last page', array(), 'sf_admin') ?]">
-        <span aria-hidden="true">&raquo;</span>
-        <span class="sr-only">[?php echo __('Last page', array(), 'sf_admin') ?]</span>
-        <i class="icon-fast-forward"></i>
-      </a>
-    </li>
-  </ul>
+        <li class="page-item">
+          <a class="page-link" href="[?php echo url_for('@<?php echo $this->getUrlForAction('list') ?>') ?]?page=[?php echo $pager->getLastPage() ?]" title="[?php echo __('Last page', array(), 'sf_admin') ?]">
+            <span aria-hidden="true">&raquo;</span>
+            <span class="sr-only">[?php echo __('Last page', array(), 'sf_admin') ?]</span>
+            <i class="icon-fast-forward"></i>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </div>
 </div>
