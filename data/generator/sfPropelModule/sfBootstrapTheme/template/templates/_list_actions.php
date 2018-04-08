ca8068365f08f6f6fbced191ff52034ddb41eb33
Hendra Saputra <Gede.Saputra@verint.com>
2018-04-08 22:24:15 +0100

    bootstrap v4.0 pagination

--- data/generator/sfPropelModule/sfBootstrapTheme/template/templates/_list_actions.php
+++ data/generator/sfPropelModule/sfBootstrapTheme/template/templates/_list_actions.php
@@ -1,13 +1,13 @@
 <?php if ($actions = $this->configuration->getValue('list.actions')): ?>
 <!-- btn-group -->
-<div class="btn-group float-right">
-<?php foreach ($actions as $name => $params): ?>
-<?php if ('_new' == $name): ?>
-<?php echo $this->addCredentialCondition('[?php echo $helper->linkToNew('.$this->asPhp($params).') ?]', $params)."\n" ?>
-<?php else: ?>
-<?php echo $this->addCredentialCondition($this->getLinkToAction($name, $params, false), $params)."\n" ?>
-<?php endif; ?>
-<?php endforeach; ?>
+<div class="btn-group">
+  <?php foreach ($actions as $name => $params): ?>
+    <?php if ('_new' == $name): ?>
+      <?php echo $this->addCredentialCondition('[?php echo $helper->linkToNew('.$this->asPhp($params).') ?]', $params)."\n" ?>
+    <?php else: ?>
+      <?php echo $this->addCredentialCondition($this->getLinkToAction($name, $params, false), $params)."\n" ?>
+    <?php endif; ?>
+  <?php endforeach; ?>
 </div>
 <!-- btn-group -->
 <?php endif; ?>
