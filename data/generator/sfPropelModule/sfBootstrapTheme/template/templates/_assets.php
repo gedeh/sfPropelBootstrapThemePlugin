079b0b83626bf8e11fbca6dd42d298989a2219db
Hendra Saputra <Gede.Saputra@verint.com>
2018-04-08 22:08:38 +0100

    automatically register asset dir and load them

--- data/generator/sfPropelModule/sfBootstrapTheme/template/templates/_assets.php
+++ data/generator/sfPropelModule/sfBootstrapTheme/template/templates/_assets.php
@@ -1,9 +1,6 @@
-<?php if (isset($this->params['css']) && ($this->params['css'] !== false)): ?> 
-[?php use_stylesheet('<?php echo $this->params['css'] ?>', 'first') ?] 
-<?php elseif(!isset($this->params['css'])): ?> 
-[?php use_stylesheet('/sfPropelBootstrapThemePlugin/css/bootstrap.min.css', 'first') ?] 
-[?php use_stylesheet('/sfPropelBootstrapThemePlugin/css/bootstrap-responsive.min.css', 'first') ?]
-[?php use_javascript('/sfPropelBootstrapThemePlugin/js/jquery-1.8.1.min.js') ?] 
-[?php use_javascript('/sfPropelBootstrapThemePlugin/js/bootstrap.min.js') ?] 
-[?php use_javascript('/sfPropelBootstrapThemePlugin/js/bootstrap-collapse.js') ?] 
+<?php if (isset($this->params['css']) && ($this->params['css'] !== false)): ?>
+[?php use_stylesheet('<?php echo $this->params['css'] ?>', 'first') ?]
+<?php elseif(!isset($this->params['css'])): ?>
+[?php use_stylesheet('<?php echo sfConfig::get('sf_twbs_web_dir').'/css/bootstrap.min.css' ?>', 'first') ?]
+[?php use_javascript('<?php echo sfConfig::get('sf_twbs_web_dir').'/js/bootstrap.min.js' ?>') ?]
 <?php endif; ?>
